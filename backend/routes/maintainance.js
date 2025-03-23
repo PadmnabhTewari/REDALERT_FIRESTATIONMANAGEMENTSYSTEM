a61a45d06d12260af980c516bd551cb679074ae4
Padmnabh Tewari <91028496+PadmnabhTewari@users.noreply.github.com>
2025-03-23 15:42:02 +0530

    Update maintainance.js

--- backend/routes/maintainance.js
+++ backend/routes/maintainance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/api/maintenance";
 
-const MaintenanceTable = () => {
+const Maintenance = () => {
   const [maintenanceData, setMaintenanceData] = useState([]);
 
   useEffect(() => {
