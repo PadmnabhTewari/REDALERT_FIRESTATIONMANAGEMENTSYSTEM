16d8bf88d2d47daa9c681b6fd3d15cad3fde51ec
Kartik Sharma <202351059@iiitvadodara.ac.in>
2025-03-24 17:39:50 +0530

    Update Clerk Key

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = 'pk_test_Y2FzdWFsLWhvcm5ldC00MS5jbGVyay5hY2NvdW50cy5kZXYk'
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
