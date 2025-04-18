f841ad9e520ed9fa5b21fadb838d45684d788385
MEET2231 <60254693+MEET2231@users.noreply.github.com>
2025-04-18 23:51:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -6,10 +7,17 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.PORT,
+  port: process.env.PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    ca: fs.readFileSync("/certs/ca.pem"),
+    // These are optional and depend on your DB provider
+    // cert: fs.readFileSync(__dirname + "/certs/client-cert.pem"),
+    // key: fs.readFileSync(__dirname + "/certs/client-key.pem"),
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool.promise();
